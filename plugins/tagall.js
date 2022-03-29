3022640c34cb6e77fffae918008f26ada72faec3
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2022-03-29 13:42:02 +0530

    Fixed TagAll

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -4,22 +4,9 @@ let Language = require('../language');
 let Config = require('../config');
 let TLang = Language.getString('tagall');
 let Lang = Language.getString('admin');
-let td = Config.WORKTYPE == 'public' ? false : true
-
- async function checkAdmin(message, user = message.data.participant) {
-    var grup = await message.client.groupMetadata(message.jid);
-    var sonuc = grup['participants'].map((member) => {     
-        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
-    });
-    return sonuc.includes(true);
-}
+let td = true
 
 Bot.addCommand({pattern: 'tagall ?(.*)', fromMe: td, desc: TLang.TAGALL_DESC, onlyGroup: true }, (async (message, match) => {
-
-if (Config.WORKTYPE == 'public') {
-    var admin = await checkAdmin(message);
-    if (!admin) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
-}
 
     if (!message.reply_message) {
         if (match[1] !== '') {
