ece302fa0d31d21de2a3969a89971669d3af028b
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2022-04-09 23:44:14 +0530

    New External Plugin - Anime 🥳

--- bot.js
+++ bot.js
@@ -161,6 +161,7 @@ ${chalk.green.bold("🔄 Connecting...")}`);
     if (config.AUTOINSTALL == 'true') {
       await plugindb.installPlugin("https://gist.github.com/TOXIC-DEVIL/62604c46e0f5f09eb60f95e3782de9b7/raw", 'dice');
       await plugindb.installPlugin("https://gist.github.com/TOXIC-DEVIL/4596c5435668b68425271a2ee2db2ba7/raw", 'slot');
+      await plugindb.installPlugin("https://gist.github.com/TOXIC-DEVIL/6047050056cc9923ba97aa59722719e5/raw", 'anime');
     }
     
     setInterval(async () => { 
