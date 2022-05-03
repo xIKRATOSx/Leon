711e56451ac60637726b0490bc045e4c99a59301
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2022-05-03 14:11:00 +0530

    Updated jid ⬆️

--- plugins/profile.js
+++ plugins/profile.js
@@ -106,18 +106,14 @@ Leon.addCommand({pattern: 'unblock ?(.*)', fromMe: true, desc: Lang.UNBLOCK_DESC
 if (Config.WORKTYPE == 'private') {
 
     Leon.addCommand({pattern: 'jid ?(.*)', fromMe: true, desc: Lang.JID_DESC}, (async (message, match) => {    
-        if (message.reply_message !== false) {
-            await message.client.sendMessage(message.jid, Lang.JID.format(message.reply_message.jid.split('@')[0], message.reply_message.jid), MessageType.text, {
-                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
-            });
+       if (message.reply_message !== false) {
+            await message.sendReply(message.reply_message.jid);
         } else if (message.mention !== false) {
             message.mention.map(async user => {
-                await message.client.sendMessage(message.jid, Lang.JID.format(user.split('@')[0], user), MessageType.text, {
-                    contextInfo: {mentionedJid: [user.replace('c.us', 's.whatsapp.net')]}
-                });    
+                await message.sendReply(user);
             });
         } else {
-            await message.client.sendMessage(message.jid, Lang.JID_CHAT.format(message.jid), MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data});
+            await message.sendReply(message.jid);
         }
     }));
 
@@ -129,18 +125,14 @@ if (Config.WORKTYPE == 'private') {
 else if (Config.WORKTYPE == 'public') {
 
     Leon.addCommand({pattern: 'jid ?(.*)', fromMe: false, desc: Lang.JID_DESC}, (async (message, match) => {    
-        if (message.reply_message !== false) {
-            await message.client.sendMessage(message.jid, Lang.JID.format(message.reply_message.jid.split('@')[0], message.reply_message.jid), MessageType.text, {
-                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
-            });
+       if (message.reply_message !== false) {
+            await message.sendReply(message.reply_message.jid);
         } else if (message.mention !== false) {
             message.mention.map(async user => {
-                await message.client.sendMessage(message.jid, Lang.JID.format(user.split('@')[0], user), MessageType.text, {
-                    contextInfo: {mentionedJid: [user.replace('c.us', 's.whatsapp.net')]}
-                });    
+                await message.sendReply(user);
             });
         } else {
-            await message.client.sendMessage(message.jid, Lang.JID_CHAT.format(message.jid), MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: message.data});
+            await message.sendReply(message.jid);
         }
     }));
 
