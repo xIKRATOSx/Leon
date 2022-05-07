e8d2ddf02b5716879c4c76c180015ae5c0075e5d
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2022-05-07 14:25:37 +0530

    Added new cv for CHAT_MODE 🥳

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ module.exports = {
     SESSION: process.env.LEON_SESSION === undefined ? '' : process.env.LEON_SESSION,
     ALIVELOGO: process.env.ALIVE_LOGO === undefined ? 'default' : process.env.ALIVE_LOGO,
     ANTILINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
+    CHATMODE: process.env.CHAT_MODE === undefined ? 'false' : process.env.CHAT_MODE,
     ANTISPAM: process.env.ANTI_SPAM === undefined ? 'false' : process.env.ANTI_SPAM,
     AUTOINSTALL: process.env.AUTO_INSTALL === undefined ? 'true' : process.env.AUTO_INSTALL,
     PVTDELMSG: process.env.PRIVATE_DELETING_MESSAGE === undefined ? 'false' : process.env.PRIVATE_DELETING_MESSAGE,
