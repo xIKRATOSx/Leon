8baf95e3c06823236d66499287db8c327a294650
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2022-05-20 16:26:52 +0530

    New version of Leon is Here 🥳

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ DATABASE_URL = process.env.DATABASE_URL === undefined ? './leon.db' : process.en
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 
 module.exports = {
-    VERSION: 'V4',
+    VERSION: 'V5',
     SESSION: process.env.LEON_SESSION === undefined ? '' : process.env.LEON_SESSION,
     ALIVELOGO: process.env.ALIVE_LOGO === undefined ? 'default' : process.env.ALIVE_LOGO,
     ANTILINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
